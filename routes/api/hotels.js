daa85058e9af173c8eb05c66d5b204e5d12de371
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-03-19 12:17:56 -0400

    updated code from 3.19

--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -3,74 +3,53 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//     res.send({msg: "Works"});
-// });
-
+//get list of available rooms 
 router.get('/', async (req, res) => {
     const name = await hotelSchema.find();
     res.send(name);
 });
 
+//get one single reservation by id 
 router.get('/:id', async (req, res) => {
-    const hotelRoom = await hotelSchema.findById(req.params.id);
+    const hotelRoom = await reservationSchema.findById(req.params.id);
     if (!hotelRoom) return res.status(404).send('The reservation with the given ID was not found.');
-    res.send(hotelRoom);
-})
-
-router.post('/', async (req, res) => {
-
-    let hotelRoom = new hotelSchema({
-        // checkIn: req.body.checkIn,
-        // checkOut: req.body.checkOut,
-        numberOfNights: req.body.numberOfNights,
-        room: req.body.room
-    });
-
-    hotelRoom = await hotelRoom.save();
-    console.log(hotelRoom); 
     res.send(hotelRoom);
 });
 
+//make a reservation
+router.post('/', async (req, res) => {
+    let hotelReservation = new reservationSchema({
+        checkIn: req.body.checkIn,
+        checkOut: req.body.checkOut,
+        numberOfNights: req.body.numberOfNights,
+        rooms: req.body.room
+    });
+
+    hotelReservation = await hotelReservation.save();
+    console.log(hotelReservation); 
+    res.send(hotelReservation);
+});
+
+//update a reservation
+router.put('/:id', async (req, res) => {
+      const hotelReservation = await reservationSchema.findByIdAndUpdate(req.params.id, {
+        checkIn: req.body.checkIn,
+        checkOut: req.body.checkOut,
+        numberOfNights: req.body.numberOfNights,
+        room: req.body.room
+    }, { new: false });
+
+    if (!hotelReservation) return res.status(404).send('The reservation with the given ID was not found.');
+
+    res.send(hotelReservation);
+    console.log("Reservation updated.")
+});
+
+//delete a reservation
 router.delete("/:id", async (req, res) => {
-    const hotelRoom = await hotelSchema.findByIdAndRemove(req.params.id);
+    const hotelRoom = await reservationSchema.findByIdAndRemove(req.params.id);
     if (!hotelRoom) return res.status(404).send(`No room available`);
     res.json(hotelRoom)
-})
-
-
-// router.delete("/:id", async (req, res) => {
-//     const message = await Message.findByIdAndRemove(req.params.id);
-//     if(!message) return res.status(404).send(`No message found`);
-//     res.json(message);
-// })
-//////---------------------------------------------------------------------------
-// router.delete("/:id", async (req, res) => {
-//     console.log("Delete request received for ID " + req.params.id);
-//     const hotelRoom = await hotelSchema.findById(req.params.id);
-//     hotelRoom.findById(req.params.id)
-//     .then(hotelRoom => hotelRoom.remonve().then(res.json({ success: "true"})))
-//     .catch(err => res.status(404).json("Not found"));
-// });
-
-// router.delete('/:id', async (req, res) => {
-//     const hotelRoom = await hotelSchema.findById(req.params.id);
-//     if (!hotelRoom) return res.status(404).send('The reservation with the given ID was not found.');
-//     res.send(hotelRoom);
-// })
-
-// router.delete('/:id', async (req, res) => {
-//     const hotelRoom = await
-// })
-
-// router.delete('/:id', getSubscriber, async (req, res) => {
-//     try {
-//         await res.subscriber.remove()
-//         res.json({ message: 'Deleted Subscriber'})
-//     } catch (err) {
-//         res.status(500).json({ message: err.message})
-//         // res.subscriber
-//     }
-// })
-
+});
+  
 module.exports = router; 
